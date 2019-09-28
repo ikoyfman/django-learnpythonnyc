4b476f0d9367b596de667f3458861664aa6c06f2
Ilya Koyfman <ilya.koy@outlook.com>
2019-09-28 12:48:14 -0400

    added pagination

--- learnpythonyc/vdata/views.py
+++ learnpythonyc/vdata/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from vdata.models import Videogame
+#pagination
+from django.core.paginator import Paginator
 
 # Create your views here.
 def vdata_view(request):
-    vdata = Videogame.objects.all()
+    vdata_list = Videogame.objects.all()
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(vdata_list, 1000)
+    try:
+        vdata = paginator.page(page)
+    except PageNotAnInteger:
+        vdata = paginator.page(1)
+    except EmptyPage:
+        vdata = paginator.page(paginator.num_pages)
 
     return render(request, 'vdata/vdata.html', context={'vdata':vdata})
